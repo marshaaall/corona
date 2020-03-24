079ae7cf633506f3cc140d69107f1eb62f920b33
marshaaall <62016990+marshaaall@users.noreply.github.com>
2020-03-24 15:46:18 +0800

    Add files via upload

--- corona.py
+++ corona.py
@@ -1,24 +1,51 @@
+from bs4 import BeautifulSoup
 import urllib.request
 import re
 
 """
-.get() function available arguments:
+.get(data) function available arguments:
     • deaths          - Deaths
     • cases           - Total Cases
     • curr_infected   - Currently Infected
     • mild_infected   - Mild conditions
     • crit_infected   - Critical conditions
     • recoveries      - Total Recoveries
+
+.get_by_country(country, data) function available arguments
+    • country - Any country affected with CVODI-19
+    • data    - cases, recovered, deaths
 """
 
-class Info():
+
+class Initialize:
     def __init__(self):
         self.web = urllib.request.urlopen('https://www.worldometers.info/coronavirus/')
+        self.web2 = urllib.request.urlopen("https://en.wikipedia.org/wiki/2019%E2%80%9320_coronavirus_pandemic_by_country_and_territory")
         self.cases = re.findall(r'[\d]{1,3},[\d]{3}', str(self.web.read()))
+        self.soup = BeautifulSoup(self.web2.read(), 'html.parser')
     
     def get(self, g):
         info = {"deaths": self.cases[1], "cases": self.cases[0], "curr_infected": self.cases[5], "mild_infected": self.cases[6], "crit_infected": self.cases[7], "recoveries": self.cases[4]}
         return info[g]
+
+    def get_by_country(self, country, d):
+        table = self.soup.get_text().strip()[8780:14470].split("\n\n\n\n\n")
+        t = {}
+        for i in table:
+            data = i.split("\n\n")[1:4]
+            c = i.split("\n\n")[0].split(r"\[.\]")[0]
+            try:
+                t[c[0:c.index('[')]] = data
+            except:
+                t[c] = data
+        if d == "cases":
+            return t[country][0]
+        elif d == "deaths":
+            return t[country][1]
+        elif d == "recoveries":
+            return t[country][2]
+        else:
+            raise ValueError("Country not found...")
 
 if __name__ == "__main__":
     print("This code is not designed to be run directly")
